479c00a2e4fc7af2cc5981dc3cfb0da6f0de257a
hassen <hassen.tn.moussi@gmail.com>
2025-07-04 22:39:52 +0100

    final push

--- services/product/src/main/java/com/esprit/pi/product/ProductController.java
+++ services/product/src/main/java/com/esprit/pi/product/ProductController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ProductController {
 
     private final ProductService service;
